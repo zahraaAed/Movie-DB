3f7fe4d292e448383d3300a16078f789065c2209
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-09-23 20:13:49 +0300

    step4

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.get('/', (req, res) => {
     
     res.status(200).json({ status: 200, message: currentTime });
   });
+  app.get('/hello/:id?', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+      res.status(200).json({ status: 200, message: `Hello, ${id}` });
+    } else {
+      res.status(200).json({ status: 200, message: 'Hello!' });
+    }
+  });
+  
+  // Route to respond with a search query or an error message
+  app.get('/search', (req, res) => {
+    const { s } = req.query;
+    if (s) {
+      res.status(200).json({ status: 200, message: 'ok', data: s });
+    } else {
+      res.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+  });
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
