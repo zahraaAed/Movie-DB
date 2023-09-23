25e55b066deb85ba611369b3bc61b936a24b771c
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-09-23 16:48:29 +0300

    step2

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app = express();
+const port = 8000;
+app.get('/', (req, res) => {
+    res.send('ok')
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
